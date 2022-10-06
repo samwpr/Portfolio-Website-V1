f880eaa225cf365218d3c80c7042c964079cdb5c
samwpr <swpr10x@gmail.com>
2022-10-06 17:03:55 +0800

    Completed About Me and Testimonial section

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 const filter_btns = document.querySelectorAll(".filter-btn");
+const skills_wrap = document.querySelector(".skills");
+const skills_bars = document.querySelectorAll(".skill-progress");
 
 filter_btns.forEach((btn) => {
   btn.addEventListener("click", () => {
@@ -18,3 +20,32 @@ $('.grid').isotope({
     layoutMode: 'fitRows',
     transitionDuration: "0.6s"
   });
+ 
+
+  window.addEventListener("scroll", () => {
+    skillsEffect();
+  });
+  
+  function checkScroll(el) {
+    let rect = el.getBoundingClientRect();
+    if (window.innerHeight >= rect.top + el.offsetHeight) return true;
+    return false;
+  }
+  
+  function skillsEffect() {
+    if (!checkScroll(skills_wrap)) return;
+    skills_bars.forEach((skill) => (skill.style.width = skill.dataset.progress));
+  }
+
+var mySwiper = new Swiper(".swiper-container", {
+  speed: 1100,
+  slidesPreview: 1,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
