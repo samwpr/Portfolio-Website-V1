7e975a8a9e620a5ec2babe3f522cea41fc4006b3
samwpr <swpr10x@gmail.com>
2022-10-07 19:41:03 +0800

    working on responsiveness

--- js/app.js
+++ js/app.js
@@ -2,6 +2,9 @@ const filter_btns = document.querySelectorAll(".filter-btn");
 const skills_wrap = document.querySelector(".skills");
 const skills_bars = document.querySelectorAll(".skill-progress");
 const footer_input = document.querySelector(".footer-input");
+const hamburger_menu = document.querySelector(".hamburger-menu");
+const navbar = document.querySelector("header nav");
+const links = document.querySelectorAll(".links a");
 
 footer_input.addEventListener("focus", () => {
   footer_input.classList.add("focus");
@@ -11,6 +14,22 @@ footer_input.addEventListener("blur", () => {
   if(footer_input.value != "") return;
   footer_input.classList.remove("focus");
 });
+
+function closeMenu() {
+  navbar.classList.remove("open");
+  document.body.classList.remove("stop-scrolling");
+}
+
+hamburger_menu.addEventListener("click", () => {
+  if (!navbar.classList.contains("open")) {
+    navbar.classList.add("open");
+    document.body.classList.add("stop-scrolling");
+  } else {
+    closeMenu();
+  }
+});
+
+links.forEach((link) => link.addEventListener("click", () => closeMenu()));
 
 
 filter_btns.forEach((btn) => {
